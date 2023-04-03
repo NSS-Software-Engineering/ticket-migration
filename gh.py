1570d788219118a63b179f56ed0cd6f1c50c55e2
Andy Collins <askingalot@gmail.com>
2023-04-03 16:37:40 -0400

    WIP - createLabel doesn't seem to exist for somet reason

--- gh.py
+++ gh.py
@@ -12,10 +12,66 @@ def get_open_issues(repo):
     return json.loads(subprocess.check_output(command))
 
 
-def create_issue(repo_id, issue):
+def get_repo(repo_with_onwer, include_issues=False):
+    owner_name, repo_name = repo_with_onwer.split('/')
+
+    command = _graphql_command(_gql_for_repo(owner_name, repo_name, include_issues))
+    response = json.loads(subprocess.check_output(command))
+
+    repository = response['data']['repository']
+    result = {
+        'id': repository['id'],
+        'name': repo_name,
+        'name_with_owner': f'{owner_name}/{repo_name}',
+        'owner_id': repository['owner']['id'],
+        'owner_name': owner_name,
+    }
+
+    if include_issues:
+        result['issues'] = _from_gql_issues(repository['issues']['nodes'])
+
+    return result
+
+
+def create_project(repo):
+    repo_id = repo['id']
+    owner_id = repo['owner_id']
+    project_title = repo['name']
+
+    command = _graphql_command(_gql_to_create_project(owner_id, repo_id, project_title))
+    response = json.loads(subprocess.check_output(command))
+
+    return {
+        'id': response['data']['createProjectV2']['projectV2']['id'],
+        'title': response['data']['createProjectV2']['projectV2']['title'],
+        'repo_id': repo_id,
+        'owner_id': owner_id
+    }
+
+
+def create_label(repo, label):
+    repo_id = repo['id']
+    name = label['name']
+    description = label['description']
+    color = label['color']
+
+    command = _graphql_command(_gql_to_create_label(repo_id, name, description, color))
+    response = json.loads(subprocess.check_output(command))
+
+    return {
+        'id': response['data']['createLabel']['label']['id'],
+        'repo_id': repo_id,
+        'name': name,
+        'description': description,
+        'color': color
+    }
+
+
+def create_issue(repo, issue):
+    repo_id = repo['id']
     title = issue['title']
     body = issue['body']
-    label_ids = []
+    label_ids = [label['id'] for label in issue['labels']]
 
     command = _graphql_command(_gql_to_create_issue(repo_id, title, body, label_ids))
     response = json.loads(subprocess.check_output(command))
@@ -42,48 +98,44 @@ def add_issue_to_project(project, issue):
     }
 
 
-def create_project(repo):
-    owner_name, repo_name = repo.split('/')
-    ids = _get_owner_and_repo_ids(owner_name, repo_name)
-    repo_id = ids['repo_id']
-    owner_id = ids['owner_id']
-
-    command = _graphql_command(_gql_to_create_project(owner_id, repo_id, repo_name))
-    response = json.loads(subprocess.check_output(command))
-
-    return {
-        'id': response['data']['createProjectV2']['projectV2']['id'],
-        'title': response['data']['createProjectV2']['projectV2']['title'],
-        'repo_id': repo_id,
-        'owner_id': owner_id
-    }
-
-
-def _get_owner_and_repo_ids(owner_name, repo_name):
-    command = _graphql_command(_gql_for_owner_and_repo_ids(owner_name, repo_name))
-    response = json.loads(subprocess.check_output(command))
-
-    return {
-        'owner_id': response['data']['repository']['owner']['id'],
-        'repo_id': response['data']['repository']['id']
-    }
-
-
 def _graphql_command(graphql):
     # Note: the last eleemnt of the command list is concatenated to avoid introducing a space
     return ['gh', 'api', 'graphql', '-f', 'query=' + graphql]
 
 
-def _gql_for_owner_and_repo_ids(owner_name, repo_name):
-    return f'''\
-query get_owner_and_repo_ids {{
+def _gql_for_repo(owner_name, repo_name, include_issues):
+    gql = f'''\
+query get_repo_with_issues {{
     repository(owner: "{owner_name}", name: "{repo_name}") {{
         id
         owner {{
             id
         }}
+'''
+
+    if include_issues:
+        gql = gql + f'''\
+        issues(first: 100, states: [OPEN]) {{
+            nodes{{
+                id
+                title
+                body
+                labels(first:100) {{
+                    nodes {{
+                        id
+                        name
+                        description
+                        color
+                    }}
+                }}
+            }}
+        }}'''
+
+    gql = gql + f'''\
     }}
 }}'''
+
+    return gql
 
 
 def _gql_to_create_project(owner_id, repo_id, title):
@@ -129,3 +181,32 @@ mutation add_project_v2_item_by_id {{
 }}'''
 
 
+def _gql_to_create_label(repo_id, name, description, color):
+    return f'''\
+mutation create_label {{
+    createLabel (input: {{
+        repositoryId: {json.dumps(repo_id)},
+        name: {json.dumps(name)},
+        description: {json.dumps(description)},
+        color: {json.dumps(color)}
+    }}) {{
+        createLabel {{
+            label {{
+                id
+            }}
+        }}
+    }}
+}}'''
+
+
+def _from_gql_issues(gql_issues):
+    return [_from_gql_issue(gi) for gi in gql_issues]
+
+
+def _from_gql_issue(gql_issue):
+    return {
+        'id': gql_issue['id'],
+        'title': gql_issue['title'],
+        'body': gql_issue['body'],
+        'labels': gql_issue['labels']['nodes']
+    }
--- migrate_tickets.py
+++ migrate_tickets.py
@@ -3,11 +3,14 @@
 import json
 import argparse
 import time
+import copy
 import gh
 
 
 def main(args):
     repos = []
+
+    source_repo = gh.get_repo(args.source_repo, include_issues=True)
 
     if args.target_repo is not None:
         repos = [args.target_repo]
@@ -15,23 +18,35 @@ def main(args):
         with open(args.config_file) as file:
             repos = json.load(file)['targetRepos']
 
-    for repo in repos:
-        migrate_tickets(args.source_repo, repo, args.throttle_seconds)
+    for repo_name_with_owner in repos:
+        migrate_tickets(source_repo, repo_name_with_owner, args.throttle_seconds)
 
 
-def migrate_tickets(source_repo, target_repo, throttle_seconds):
+def migrate_tickets(source_repo, target_repo_name_with_onwer, throttle_seconds):
+    target_repo = gh.get_repo(target_repo_name_with_onwer)
     target_project = gh.create_project(target_repo)
+
     time.sleep(throttle_seconds)
 
-    issues = gh.get_open_issues(source_repo)
-    time.sleep(throttle_seconds)
+    source_issues = source_repo['issues']
+    source_labels = get_unique_labels(source_issues)
 
-    for issue in issues:
-        new_issue = gh.create_issue(target_project['repo_id'], issue)
-        gh.add_issue_to_project(target_project, new_issue)
+    source_to_target_labels = {}
+    for source_label in source_labels:
+        target_label = gh.create_label(target_repo, source_label)
+        source_to_target_labels[source_label['id']] = target_label
+
         time.sleep(throttle_seconds)
 
-    print(f'Tickets migrated for {target_repo}!')
+    with_new_labels = transform_with_new_labels(source_issues, source_to_target_labels)
+
+    for source_issue in with_new_labels:
+        target_issue = gh.create_issue(target_repo, source_issue)
+        gh.add_issue_to_project(target_project, target_issue)
+
+        time.sleep(throttle_seconds)
+
+    print(f'Tickets migrated for {target_repo_name_with_onwer}!')
 
 
 def get_args():
@@ -57,6 +72,28 @@ def get_args():
                        type=str)
 
     return parser.parse_args()
+
+
+def get_unique_labels(issues):
+    label_dict = {
+        label['id']: label
+        for issue in issues
+        for label in issue['labels']
+    }
+    return label_dict.values()
+
+
+def transform_with_new_labels(issues, label_dict):
+    tranformed = []
+
+    for issue in issues:
+        new_issue = copy.copy(issue)
+        new_issue['labels'] = [
+            label_dict[old_label['id']] for old_label in issue['labels']
+        ]
+        tranformed.add(new_issue)
+
+    return tranformed
 
 
 if __name__ == '__main__':
